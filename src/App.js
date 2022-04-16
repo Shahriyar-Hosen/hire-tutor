c54a0ead2e5e794f76938d90e48a3b37711925b6
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-04-17 02:24:00 +0600

    route setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./Components/Home/Home";
+import Footer from "./Components/Shared/Footer/Footer";
+import Header from "./Components/Shared/Header/Header";
 
 function App() {
   return (
     <div>
-      
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/home" element={<Home />}></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
--- src/Components/Shared/Footer/Footer.js
+++ src/Components/Shared/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-            <h1>This is Footer page</h1>
+            <p><small>This is Footer page</small></p>
         </div>
     );
 };
