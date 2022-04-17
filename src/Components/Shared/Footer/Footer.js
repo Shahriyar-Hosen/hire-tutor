558e4992555bc3f59d0ab4211b078525029b352b
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-04-17 15:45:16 +0600

    Footer add & all style add in MDB

--- src/Components/Shared/Footer/Footer.js
+++ src/Components/Shared/Footer/Footer.js
@@ -1,11 +1,75 @@
-import React from 'react';
+import React from "react";
+import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
-    return (
-        <div>
-            <p><small>This is Footer page</small></p>
+  return (
+    <div>
+      <MDBFooter color="black" className="font-small pt-4 mt-4">
+        <MDBContainer fluid className="text-center text-md-left container">
+          <MDBRow>
+            <MDBCol md="6">
+              <h2 className="title">Connect</h2>
+              <h5>Phone: 015xxxxxxxx</h5>
+              <h5>
+                <span>Address: </span>
+                <small>
+                  Flat 3A, House 26, <br /> Road Alawul Avenue, <br />
+                  Sector 6, Uttara Dhaka, <br />
+                  Bangladesh
+                </small>
+              </h5>
+            </MDBCol>
+            <MDBCol md="6">
+              <h2 className="title my-3">Social</h2>
+              <div>
+                <p className="">
+                  <Link to="https://www.facebook.com/">
+                    <img
+                      src="https://i.ibb.co/yhLpRSJ/fb-icon.png"
+                      style={{ height: "20px" }}
+                      className="mx-2"
+                      alt=""
+                    />{" "}
+                    Facebook
+                  </Link>
+                </p>
+                <p className="">
+                  <Link to="https://www.linkedin.com/">
+                    {" "}
+                    <img
+                      src="https://i.ibb.co/hLCyjxJ/in-icon.png"
+                      style={{ height: "20px" }}
+                      className="mx-2"
+                      alt=""
+                    />{" "}
+                    Linkedin
+                  </Link>
+                </p>
+                <p className="">
+                  <Link to="https://www.youtube.com/">
+                    <img
+                      src="https://i.ibb.co/qrz1Vvb/You-Tube-icon.webp"
+                      style={{ height: "15px" }}
+                      className="mx-2"
+                      alt=""
+                    />{" "}
+                    Youtube
+                  </Link>
+                </p>
+              </div>
+            </MDBCol>
+          </MDBRow>
+        </MDBContainer>
+        <div className="footer-copyright text-center py-3">
+          <MDBContainer fluid>
+            &copy; {new Date().getFullYear()} Copyright:{" "}
+            <a href="https://www.mdbootstrap.com"> Hire Tutor </a>
+          </MDBContainer>
         </div>
-    );
+      </MDBFooter>
+    </div>
+  );
 };
 
 export default Footer;
--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom";
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
