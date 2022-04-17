e1f9e0a6c3863b54f827991d85402c5a9ae9d268
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-04-17 17:56:35 +0600

    Signup & Login page all style done

--- src/Components/User/Login/Login.js
+++ src/Components/User/Login/Login.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { Button, Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
-// import SocialLogin from "../SocialLogin/SocialLogin";
+import SocialLogin from "../SocialLogin/SocialLogin";
+import login from "../../../images/Login/Login.jpg";
+import user from "../../../images/Login/user.jpg";
 
 const Login = () => {
   const handleSubmit = (event) => {
@@ -10,48 +12,76 @@ const Login = () => {
     // signInWithEmailAndPassword(email, password);
   };
   return (
-    <div className="w-50 mx-auto">
-      <h1 className="text-primary text-center mt-2">Please Login</h1>
-      <Form onSubmit={handleSubmit}>
-        <Form.Group className="mb-3 mt-3" controlId="formBasicEmail">
-          <Form.Control type="email" placeholder="Enter email" />
-        </Form.Group>
+    <div className="container mb-5">
+      <div className="w-100">
+        <img
+          style={{ height: "300px" }}
+          src={user}
+          className="img-fluid d-block mx-auto"
+          alt=""
+        />
+      </div>
+      <div class="card mb-3  border-0">
+        <div class="row g-0">
+          <div class="col-md-6">
+            <img
+              src={login}
+              class="img-fluid rounded-start d-md-block d-none"
+              alt="..."
+            />
+          </div>
+          <div class="col-md-6">
+            <div class="card-body">
+              <div className="w-100 mx-auto">
+                <h1 className="text-primary text-center mt-2">Please Login</h1>
+                <Form onSubmit={handleSubmit}>
+                  <Form.Group className="mb-3 mt-3" controlId="formBasicEmail">
+                    <Form.Control type="email" placeholder="Enter email" />
+                  </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        {/* <p>{error?.message}</p>
+                  <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Control type="password" placeholder="Password" />
+                  </Form.Group>
+                  {/* <p>{error?.message}</p>
         <p>{resetError?.message}</p> */}
-        <Form.Group className="mb-3" controlId="formBasicCheckbox"></Form.Group>
-        <Button
-          className="w-100 rounded-pill d-block mx-auto mb-4"
-          variant="primary"
-          type="submit"
-        >
-          Login
-        </Button>
-      </Form>
-      <p className=" text-center mt-2">
-        Now to Genius car?
-        <Link
-          to="/register"
-          //   onClick={handleNavigate}
-          className="text-primary text-decoration-none"
-        >
-          Please Register
-        </Link>
-      </p>
-      <p className=" text-center mt-2">
-        Forget Password?
-        <Link
-          to="/login"
-          //   onClick={resetPassword}
-          className="text-primary text-decoration-none"
-        >
-          Reset Password
-        </Link>
-      </p>
-      {/* <SocialLogin></SocialLogin> */}
+                  <Form.Group
+                    className="mb-3"
+                    controlId="formBasicCheckbox"
+                  ></Form.Group>
+                  <Button
+                    className="w-100 rounded-pill d-block mx-auto mb-4"
+                    variant="primary"
+                    type="submit"
+                  >
+                    Login
+                  </Button>
+                </Form>
+                <p className=" text-center mt-2">
+                  New User?
+                  <Link
+                    to="/register"
+                    //   onClick={handleNavigate}
+                    className="text-primary text-decoration-none px-2"
+                  >
+                    Please Register
+                  </Link>
+                </p>
+                <p className=" text-center mt-2">
+                  Forget Password?
+                  <Link
+                    to="/login"
+                    //   onClick={resetPassword}
+                    className="text-primary text-decoration-none px-2"
+                  >
+                    Reset Password
+                  </Link>
+                </p>
+                <SocialLogin></SocialLogin>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/Components/User/Signup/Signup.js
+++ src/Components/User/Signup/Signup.js
@@ -1,11 +1,87 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import SocialLogin from "../SocialLogin/SocialLogin";
+import signup1 from "../../../images/Login/Signup.jpg";
+import signup2 from "../../../images/Login/sign-up.jpg";
 
 const Signup = () => {
-    return (
-        <div>
-            <h1>This is sign up page</h1>
+    const [agree, setAgree] = useState(false);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <div className="container mb-5">
+      <div className="w-100">
+        <img
+          style={{ height: "300px" }}
+          src={signup2}
+          className="img-fluid d-block mx-auto m-5"
+          alt=""
+        />
+      </div>
+      <div class="card mb-3  border-0">
+        <div class="row g-0">
+          <div class="col-md-6">
+            <img
+              src={signup1}
+              class="img-fluid rounded-start d-md-block d-none"
+              alt="..."
+            />
+          </div>
+          <div class="col-md-6">
+            <div class="card-body">
+              <div className="w-100 mx-auto">
+                <h1 className="text-primary text-center mt-2">
+                  Please Sign Up
+                </h1>
+                <Form onSubmit={handleSubmit}>
+                  <Form.Group className="mb-3 mt-3" controlId="formBasicName">
+                    <Form.Control type="text" placeholder="Enter your name" />
+                  </Form.Group>
+                  <Form.Group className="mb-3 mt-3" controlId="formBasicEmail">
+                    <Form.Control type="email" placeholder="Enter email" />
+                  </Form.Group>
+
+                  <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Control type="password" placeholder="Password" />
+                  </Form.Group>
+                  {/* <p>{error?.message}</p>
+        <p>{resetError?.message}</p> */}
+                  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check
+                      onClick={() => setAgree(!agree)}
+                      className={agree ? "text-success" : ""}
+                      type="checkbox"
+                      label="Accept Genius Car Terms and Condition"
+                    />
+                  </Form.Group>
+                  <Button
+                    className="w-100 rounded-pill d-block mx-auto mb-4"
+                    variant="primary"
+                    type="submit"
+                  >
+                    Login
+                  </Button>
+                </Form>
+                <p className=" text-center mt-2">
+                Already have an account?
+                  <Link
+                    to="/register"
+                    //   onClick={handleNavigate}
+                    className="text-primary text-decoration-none px-2"
+                  >
+                    Please Login
+                  </Link>
+                </p>
+                <SocialLogin></SocialLogin>
+              </div>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Signup;
